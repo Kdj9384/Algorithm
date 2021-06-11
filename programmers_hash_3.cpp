4119683263f3f2475fe56b5b51483837cf1c0045
Kdj9394 <kdj93843404@gamil.com>
2021-06-11 16:38:37 +0900

    programmers_hash_3 Day2

--- programmers_hash_3.cpp
+++ programmers_hash_3.cpp
@@ -9,55 +9,39 @@ using namespace std;
 
 int mySolution(vector<vector<string>> clothes)
 {
-  int answer = 0;
-  multimap<string, string> multimap1;
-  set<string> set1;
   map<string, int> map1;
-
+  int cloth_type_num;
   int length = clothes.size();
-  printf("입력 개수 : %d\n", length);
+  int total = 1;
+  int temp = 0;
 
   for (auto i : clothes)
   {
-    cout << "옷의 종료 : " << i[1] << "\n";
-
     int cnt = map1.count(i[1]);
 
     if (cnt)
     {
-      map1[i[1]] = 1 + cnt;
+      map1[i[1]] += 1;
     }
     else
     {
-      map1.insert(make_pair(i[1], 1 + cnt));
+      map1.insert(make_pair(i[1], 1));
     }
   }
 
   // 옷의 종류의 개수
-  int cloth_type_num = map1.size();
-  cout << cloth_type_num << "\n";
+  cloth_type_num = map1.size();
 
-  // 옷의 종류 - 개수 로 매핑 되어있음.
-  // 조합하는 알고리즘 개발 필요.
-  int total = 0;
-
-  // 1 ~ n개를 입을 경우,
-  for (int i = 1; i <= cloth_type_num; i++)
+  // 각 옷의 종류에, 옷을 안 입는 종류 추가.
+  for (auto i : map1)
   {
-    if (i == 1)
-    {
-      // 1개만 입을 경우,
-      for (auto i : map1)
-      {
-        total += i.second;
-      }
-    }
-    else
-    {
-    }
+    total *= i.second + 1;
   }
 
-  return answer;
+  // 모든 종류의 옷을 안입는 경우 제외.
+  total -= 1;
+
+  return total;
 }
 
 int solution(vector<vector<string>> clothes)
@@ -71,6 +55,6 @@ int solution(vector<vector<string>> clothes)
 
 int main()
 {
-  vector<vector<string>> clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
+  vector<vector<string>> clothes = {{"yellowhat", "face"}, {"bluesunglasses", "face"}, {"green_turban", "face"}};
   int answer = solution(clothes);
 }
