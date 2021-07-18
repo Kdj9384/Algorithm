ea6010b8db990e4fed56720f8b848f674b269239
Kdj9394 <kdj93843404@gamil.com>
2021-07-18 19:22:38 +0900

    programmers_binary_search Day 1 Fail

---
+++ BINARY_SEARCH/programmers_binary_search_2.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int distance, vector<int> rocks, int n)
+{
+  int answer = 0;
+  int min, max;
+
+    return answer;
+}
+
+int main()
+{
+  int distance = 25;
+  vector<int> rocks = {2, 14, 11, 21, 17};
+  int n = 2;
+}
